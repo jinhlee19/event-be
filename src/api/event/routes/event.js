7580c598cc9cd5efe6fe45708f02d116cdbac866
jinhlee19 <jinhlee19@gmail.com>
2023-01-29 22:52:40 +0900

    strapi v4 controller and route edit update

--- src/api/event/routes/event.js
+++ src/api/event/routes/event.js
@@ -1,9 +1,19 @@
-'use strict';
+"use strict";
 
 /**
  * event router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::event.event');
+// module.exports = createCoreRouter("api::event.event");
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/events/me",
+      handler: "event.me",
+      config: {},
+    },
+  ],
+};
