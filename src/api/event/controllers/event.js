61e268fb6da52cabd68ee495a7c1f61ce6bc5465
jinhlee19 <jinhlee19@gmail.com>
2023-02-01 20:22:26 +0900

    .

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -1,37 +1,80 @@
+// "use strict";
+
+// // ! Strapi Base
+// // const { createCoreController } = require("@strapi/strapi").factories;
+// // module.exports = createCoreController("api::event.event");
+
+// // ! Custom Events Endpoint - me
+// const { createCoreController } = require("@strapi/strapi").factories;
+// module.exports = createCoreController("api::event.event", ({ strapi }) => ({
+//   // Get logged in users
+//   async me(ctx) {
+//     const user = ctx.state.user;
+
+//     if (!user) {
+//       return ctx.badRequest(null, [
+//         { message: "No authorization header was found" },
+//       ]);
+//     }
+
+//     const data = await strapi.db.query("api::event.event").findMany({
+//       where: {
+//         user: { id: user.id },
+//       },
+//       populate: { user: true, image: true },
+//     });
+//     console.log("userID", user.id);
+//     if (!data) {
+//       return ctx.notFound();
+//     }
+
+//     const res = await this.sanitizeOutput(data, ctx);
+//     return res;
+//   },
+// }));
+
+// // * Reference
+// // https://docs.strapi.io/developer-docs/latest/development/backend-customization/controllers.html#attaching-a-controller-to-a-route
+
+//! 테스팅 중 //
+
 "use strict";
 
-// ! Strapi Base
-// const { createCoreController } = require("@strapi/strapi").factories;
-// module.exports = createCoreController("api::event.event");
+/**
+ *  event controller
+ */
 
-// ! Custom Events Endpoint - me
 const { createCoreController } = require("@strapi/strapi").factories;
+
 module.exports = createCoreController("api::event.event", ({ strapi }) => ({
   // Get logged in users
   async me(ctx) {
     const user = ctx.state.user;
 
-    if (!user) {
-      return ctx.badRequest(null, [
-        { message: "No authorization header was found" },
-      ]);
-    }
+    console.log(user);
 
-    const data = await strapi.db.query("api::event.event").findMany({
-      where: {
-        user: { id: user.id },
-      },
-      populate: { user: true, image: true },
-    });
-    console.log("userID", user.id);
-    if (!data) {
-      return ctx.notFound();
-    }
+    // if (!user) {
+    //   return ctx.badRequest(null, [
+    //     { message: "No authorization header was found" },
+    //   ]);
+    // }
+    // // const data = await strapi.db.query("api::event.event").findOne({
+    // //   where: {
+    // //     user: { id: user.id },
+    // //   },
+    // //   populate: { user: true, image: true },
+    // // });
+
+    // // ? Testing
+    // const { data } = await super.find(ctx);
+    // console.log(data);
+    // // ? Test End
+
+    // if (!data) {
+    //   return ctx.notFound();
+    // }
 
     const res = await this.sanitizeOutput(data, ctx);
     return res;
   },
 }));
-
-// * Reference
-// https://docs.strapi.io/developer-docs/latest/development/backend-customization/controllers.html#attaching-a-controller-to-a-route
